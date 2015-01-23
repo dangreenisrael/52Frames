47d786de68fb9f2f33ac40e3a8ec2509cd3c2199
Ronen Rashty <ronenrer@gmail.com>
2015-01-23 12:24:04 +0200

    album page design continue

--- wp-content/themes/toolset-bootstrap-child/content-albums_collection.php
+++ wp-content/themes/toolset-bootstrap-child/content-albums_collection.php
@@ -4,40 +4,29 @@
  *
  */
 ?>
-
-
-<!--<div class="raw">-->
+<?php $nudity = get_post_meta( get_the_id(), 'wpcf-this-photo-contains-nudity', true);
+	$nude = '';
+	if ($nudity != 0) $nude = " nude" ;
+?>
 <div class="span3">
 	<article <?php post_class('clearfix') ?> id="post-<?php the_ID(); ?>">
-	<!--<div class="col-md-8">-->
-		<div class="photo-thumbnail">
-			<div class="entry-content clearfix">
-				<?php $nudity = get_post_meta( get_the_id(), 'wpcf-this-photo-contains-nudity', true);
-					$nude = '';
-					if ($nudity != 0) $nude = " nude" ?>
-					<?php if ( has_post_thumbnail() && wpbootstrap_get_setting('general_settings','display_thumbnails') ): ?>
-						<a href="<?php the_permalink(); ?>" class="post-thumbnail thumbnail pull-left<?php echo $nude ?>">
-							<?php the_post_thumbnail('thumb-480'); ?>
-							<?php if (function_exists('exifography_display_exif')) exifography_display_exif($options); ?>
-							
-						</a>
-					<?php endif; ?>
-				
-			</div>
-			<div class="photo-meta">
-			<p>
-				<?php /* echo <a href="get_author_posts_url( get_the_author_meta( 'ID' ) ) rel="author">*/
+		<figure>
+			<a href="<?php the_permalink(); ?>" class="post-thumbnail thumbnail<?php echo $nude ?>">
+				<?php the_post_thumbnail('thumb-480'); ?>			
+			</a>
+			<figcaption>		
+				<h2><?php /* echo <a href="get_author_posts_url( get_the_author_meta( 'ID' ) ) rel="author">*/
 					echo get_the_author(); ?>
-			</p>
-			<?php if( function_exists('zilla_likes') ) zilla_likes(); ?>
-			<i class="fa fa-comments"></i><?php comments_popup_link( __( '<span>0</span>', 'wpbootstrap' ), __( '<span>1</span>', 'wpbootstrap' ), __( '<span>%</span>', 'wpbootstrap' ) );?>
-			<h4 class="photo-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-	
-				<?php if ( current_user_can( 'judge' ) ) : ?>
-					<div class="rating">
-					<?php if(function_exists('the_ratings')) { the_ratings(); } ?>
-					</div>
-				<?php endif ?>	
-			</div> 
+				<?php if( function_exists('zilla_likes') ) zilla_likes();?>
+				<i class="fa fa-comments"></i><?php comments_popup_link( __( '<span>0</span>', 'wpbootstrap' ), __( '<span>1</span>', 'wpbootstrap' ), __( '<span>%</span>', 'wpbootstrap' ) );?>
+				</h2>
+				<p><a href=<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
+			</figcaption>			
+		</figure>	
+		<?php if ( current_user_can( 'judge' ) ) : ?>
+			<div class="rating">
+			<?php if(function_exists('the_ratings')) { the_ratings(); } ?>
+			</div>
+		<?php endif ?>	
 	</article>
 </div>
--- wp-content/themes/toolset-bootstrap-child/taxonomy-photo_alboms.php
+++ wp-content/themes/toolset-bootstrap-child/taxonomy-photo_alboms.php
@@ -18,35 +18,48 @@ endwhile; endif;
 	?>
 
 <div class="row-fluid winners">
-	<div class="container album-list">
+	<div class="container">
 		<div class="span4 winner">
-			<?php echo get_the_post_thumbnail($w_arr['Winner'], 'thumb-480'); 
-			 echo '<a href="'.get_the_permalink($w_arr['Winner']).'">'.get_the_title($w_arr['Winner']).'</a>';
-			 echo '<span class="badge winner">Winner</span>';
-			 //echo '<a href="'.get_author_posts_url( $w_arr['Winner'],get_the_author_meta( 'ID' ) ) .'" rel="author">';
-			 echo get_the_author_meta('display_name', get_post($w_arr['Winner'])->post_author); 
-			 if( function_exists('zilla_likes') ) zilla_likes( $w_arr['Winner']); 
-
-			?>				
+			<figure>		
+			<?php echo get_the_post_thumbnail($w_arr['Winner'],'thumb-480'); ?>
+				<span class="badge winner">Winner</span>		
+				<figcaption>		
+					<h2><!--a href="'.get_author_posts_url( $w_arr['Winner'],get_the_author_meta( 'ID' ) ) .'" rel="author"-->
+					 <?php echo  get_the_author_meta('display_name', get_post($w_arr['Winner'])->post_author);
+					if( function_exists('zilla_likes') ) zilla_likes($w_arr['Winner']);?>
+					<i class="fa fa-comments"></i><?php comments_popup_link($w_arr['Winner'], __( '<span>0</span>', 'wpbootstrap' ), __( '<span>1</span>', 'wpbootstrap' ), __( '<span>%</span>', 'wpbootstrap' ) );?>
+					</h2>
+					<p><a href="<?php echo get_the_permalink($w_arr['Winner'])?>"><?php echo get_the_title($w_arr['Winner'])?></a></p>
+				</figcaption>			
+			</figure>			
 		</div>
 		<div class="span4 1runner">
-			<?php echo get_the_post_thumbnail($w_arr['1st Runner-up'],'thumb-480');
-			 echo '<a href="'.get_the_permalink($w_arr['1st Runner-up']).'">'.get_the_title($w_arr['1st Runner-up']).'</a>';
-			 echo '<span class="badge runner-up">1st Runner-up</span>';
-			 //echo '<a href="'.get_author_posts_url( $w_arr['1st Runner-up'],get_the_author_meta( 'ID' ) ) .'" rel="author">';
-			 echo get_the_author_meta('display_name', get_post($w_arr['1st Runner-up'])->post_author); 
-			 if( function_exists('zilla_likes') ) zilla_likes( $w_arr['1st Runner-up']); 
-			 ?>
+			<figure>		
+			<?php echo get_the_post_thumbnail($w_arr['1st Runner-up'],'thumb-480'); ?>
+				<span class="badge winner">1st Runner-up</span>		
+				<figcaption>		
+					<h2><!--a href="'.get_author_posts_url( $w_arr['1st Runner-up'],get_the_author_meta( 'ID' ) ) .'" rel="author"-->
+					 <?php echo  get_the_author_meta('display_name', get_post($w_arr['1st Runner-up'])->post_author);
+					if( function_exists('zilla_likes') ) zilla_likes($w_arr['1st Runner-up']);?>
+					<i class="fa fa-comments"></i><?php comments_popup_link($w_arr['1st Runner-up'], __( '<span>0</span>', 'wpbootstrap' ), __( '<span>1</span>', 'wpbootstrap' ), __( '<span>%</span>', 'wpbootstrap' ) );?>
+					</h2>
+					<p><a href="<?php echo get_the_permalink($w_arr['1st Runner-up'])?>"><?php echo get_the_title($w_arr['1st Runner-up'])?></a></p>
+				</figcaption>			
+			</figure>		
 		</div>
 		<div class="span4 2runner">
-			<?php echo get_the_post_thumbnail($w_arr['2nd Runner-up'],'thumb-480');
-			echo '<a href="'.get_the_permalink($w_arr['2nd Runner-up']).'">'.get_the_title($w_arr['2nd Runner-up']).'</a>';
-			 echo '<span class="badge runner-up">2nd Runner-up</span>';
-			 //echo '<a href="'.get_author_posts_url( $w_arr['2nd Runner-up'],get_the_author_meta( 'ID' ) ) .'" rel="author">';
-			 echo get_the_author_meta('display_name', get_post($w_arr['2nd Runner-up'])->post_author); 
-			 if( function_exists('zilla_likes') ) zilla_likes( $w_arr['2nd Runner-up']); 
-			 ?>
-
+			<figure>		
+			<?php echo get_the_post_thumbnail($w_arr['2nd Runner-up'],'thumb-480'); ?>
+				<span class="badge winner">2nd Runner-up</span>		
+				<figcaption>		
+					<h2><!--a href="'.get_author_posts_url( $w_arr['2nd Runner-up'],get_the_author_meta( 'ID' ) ) .'" rel="author"-->
+					 <?php echo  get_the_author_meta('display_name', get_post($w_arr['2nd Runner-up'])->post_author);
+					if( function_exists('zilla_likes') ) zilla_likes($w_arr['2nd Runner-up']);?>
+					<i class="fa fa-comments"></i><?php comments_popup_link($w_arr['2nd Runner-up'], __( '<span>0</span>', 'wpbootstrap' ), __( '<span>1</span>', 'wpbootstrap' ), __( '<span>%</span>', 'wpbootstrap' ) );?>
+					</h2>
+					<p><a href="<?php echo get_the_permalink($w_arr['2nd Runner-up'])?>"><?php echo get_the_title($w_arr['2nd Runner-up'])?></a></p>
+				</figcaption>			
+			</figure>		
 		</div>
 	</div>
 </div>
@@ -67,7 +80,7 @@ endwhile; endif;
 	</div>
 
 	<?php if ( have_posts() ) : ?>
-		<div class="row-fluid">
+		<div class="row-fluid album-lists">
 			<?php
 				while ( have_posts() ) : the_post();
 					get_template_part( 'content-albums_collection' );
